f2287ae0cdc46b70bccf2ef2667097227c1c0f3e
Ke Jie <chzealot@gmail.com>
2023-09-07 09:49:08 +0800

    fix: typo issue
    Signed-off-by: Ke Jie <chzealot@gmail.com>

--- dingtalksd.py
+++ dingtalksd.py
@@ -214,7 +214,7 @@ def main():
     credential = dingtalk_stream.Credential(options.client_id, options.client_secret)
     client = dingtalk_stream.DingTalkStreamClient(credential, logger=logger)
 
-    client.register_callback_hanlder(dingtalk_stream.chatbot.ChatbotMessage.TOPIC,
+    client.register_callback_handler(dingtalk_stream.chatbot.ChatbotMessage.TOPIC,
                                      StableDiffusionBot(options, logger=logger))
     client.start_forever()
 
